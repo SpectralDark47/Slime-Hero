a9a93894dc24b0366bd97563d3071ea17f09e631
Colton Ward <wardc0714@outlook.com>
2024-05-08 08:35:38 -0500

    Disappearing enemies bug gone
    DeltaTime issue is still there though

--- core/src/com/mygdx/slimehero/SlimeHero.java
+++ core/src/com/mygdx/slimehero/SlimeHero.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -24,12 +25,17 @@ public class SlimeHero extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Rectangle hero;
 	private Rectangle enemy;
+	private Array<Rectangle> enemies;
+	private Texture backgroundTexture;
+	private long enemySpawnTime;
+	private int hitCount = 0;
 
 	@Override
 	public void create () {
 		// load the images for the hero and an enemy, 32x32 pixels each
-		heroImage = new Texture(Gdx.files.internal("Player_walk_cycle-1.png"));
-		//enemyImage = new Texture(Gdx.files.internal("bucket.png"));
+		heroImage = new Texture(Gdx.files.internal("hero.png"));
+		enemyImage = new Texture(Gdx.files.internal("enemy.png"));
+		backgroundTexture = new Texture(Gdx.files.internal("Background_dirt.png"));
 
 		// a Camera and a SpriteBatch
 		camera = new OrthographicCamera();
@@ -39,23 +45,38 @@ public class SlimeHero extends ApplicationAdapter {
 
 		// adding the Hero
 		hero = new Rectangle();
-		hero.x = 360 / 2 - 16 / 2;
-		hero.y = 20;
-		hero.width = 16;
+		hero.x = 360 / 2 - 32 / 2;
+		hero.y = 16;
+		hero.width = 32;
 		hero.height = 32;
 
 		enemy = new Rectangle();
+		enemy.width = 32;
+		enemy.height = 32;
+
+		enemies = new Array<Rectangle>();
+		spawnEnemy();
+	}
+
+	private void spawnEnemy() {
+		enemy.x = MathUtils.random(0, 360 - 32);
+		enemy.y = 640;
+		enemies.add(enemy);
+		enemySpawnTime = TimeUtils.nanoTime();
 	}
 
 	@Override
 	public void render () {
 		// rendering the Bucket
-		ScreenUtils.clear(0,0,0.2f,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(backgroundTexture,0,0,360,640);
 		batch.draw(heroImage, hero.x, hero.y);
-		//batch.draw(enemyImage, enemy.x, enemy.y);
+		for(Rectangle enemy: enemies){
+			batch.draw(enemyImage, enemy.x, enemy.y);
+		}
 		batch.end();
 
 		// making the Hero move (Touch/Mouse)
@@ -66,11 +87,21 @@ public class SlimeHero extends ApplicationAdapter {
 			hero.x = touchPos.x - 64/2;
 		}
 
-		// making the Hero move (Keyboard)
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) hero.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) hero.x += 200 * Gdx.graphics.getDeltaTime();
 		if(hero.x < 0) hero.x = 0;
 		if(hero.x > 800 - 64) hero.x = 800-64;
+		if(TimeUtils.nanoTime() - enemySpawnTime > 100000000) spawnEnemy();
+		for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext(); ) {
+			Rectangle enemy = iter.next();
+			enemy.y -= 200 * Gdx.graphics.getDeltaTime();
+
+			if(enemy.y + 64 < 0) iter.remove();
+		}
+
+		if (hero.overlaps(enemy)){
+			hitCount++;
+			Gdx.app.log("Collision", "Hit Count: " + hitCount);
+		}
+
 	}
 
 	@Override
