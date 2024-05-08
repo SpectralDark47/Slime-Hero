76f42fbd4c4983bb04e140c28e8d3e9e613902c8
Colton Ward <wardc0714@outlook.com>
2024-05-08 07:02:15 -0500

    Enemies randomly generate and fall

--- core/src/com/mygdx/slimehero/SlimeHero.java
+++ core/src/com/mygdx/slimehero/SlimeHero.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,6 +15,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.Logger;
 
 import java.util.Iterator;
 
@@ -24,12 +26,15 @@ public class SlimeHero extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Rectangle hero;
 	private Rectangle enemy;
-
+	private Array<Rectangle> enemies;
+	private Texture backgroundTexture;
+	private long enemySpawnTime;
 	@Override
 	public void create () {
 		// load the images for the hero and an enemy, 32x32 pixels each
-		heroImage = new Texture(Gdx.files.internal("Player_walk_cycle-1.png"));
-		enemyImage = new Texture(Gdx.files.internal("Basic_enemy_walk_cycle-1.png"));
+		heroImage = new Texture(Gdx.files.internal("hero.png"));
+		enemyImage = new Texture(Gdx.files.internal("enemy.png"));
+		backgroundTexture = new Texture(Gdx.files.internal("Background_dirt.png"));
 
 		// a Camera and a SpriteBatch
 		camera = new OrthographicCamera();
@@ -39,27 +44,39 @@ public class SlimeHero extends ApplicationAdapter {
 
 		// adding the Hero
 		hero = new Rectangle();
-		hero.x = 360 / 2 - 16 / 2;
-		hero.y = 20;
-		hero.width = 16;
+		hero.x = 360 / 2 - 32 / 2;
+		hero.y = 16;
+		hero.width = 32;
 		hero.height = 32;
 
 		enemy = new Rectangle();
-		enemy.x = 180 / 2 - 16 / 2;
-		enemy.y = 20;
 		enemy.width = 32;
 		enemy.height = 32;
+
+
+		enemies = new Array<Rectangle>();
+		spawnEnemy();
+	}
+
+	private void spawnEnemy() {
+		enemy.x = MathUtils.random(0, 360-32);
+		enemy.y = 640;
+		enemies.add(enemy);
+		enemySpawnTime = TimeUtils.nanoTime();
 	}
 
 	@Override
 	public void render () {
 		// rendering the Bucket
-		ScreenUtils.clear(0,0,0.2f,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		batch.draw(backgroundTexture,0,0,360,640);
 		batch.draw(heroImage, hero.x, hero.y);
-		batch.draw(enemyImage, enemy.x, enemy.y);
+		for(Rectangle enemy: enemies){
+			batch.draw(enemyImage, enemy.x, enemy.y);
+		}
 		batch.end();
 
 		// making the Hero move (Touch/Mouse)
@@ -70,23 +87,31 @@ public class SlimeHero extends ApplicationAdapter {
 			hero.x = touchPos.x - 64/2;
 		}
 
-		// making the Hero move (Keyboard)
-		int moveAmount = 0;
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) moveAmount -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) moveAmount += 200 * Gdx.graphics.getDeltaTime();
 		//handle collision with enemy and finish movement
 		float heroPrevX = hero.x;
-		hero.x += moveAmount;
 		if(hero.overlaps(enemy)) hero.x = heroPrevX;
 
 		if(hero.x < 0) hero.x = 0;
 		if(hero.x > 800 - 64) hero.x = 800-64;
+		if(TimeUtils.nanoTime() - enemySpawnTime > 100000000/0.4) spawnEnemy();
+
+		for (Iterator<Rectangle> iter = enemies.iterator(); iter.hasNext(); ) {
+			Rectangle enemy = iter.next();
+			enemy.y -= 200 * Gdx.graphics.getDeltaTime();
+
+			if(enemy.y < 0) iter.remove();
+		}
+
+
 	}
 
 	@Override
 	public void dispose () {
 		enemyImage.dispose();
 		heroImage.dispose();
+		backgroundTexture.dispose();
 		batch.dispose();
 	}
+
+
 }
